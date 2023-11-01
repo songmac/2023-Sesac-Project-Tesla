eea6f643a8054187da0556fc17a789fae6984402
yumi oh <13yooms@gmail.com>
2023-11-01 15:27:12 +0900

    update : 테슬라 일별 주식 병합 함수 추가

--- merge/code/mergeCode.py
+++ merge/code/mergeCode.py
@@ -37,3 +37,30 @@ def call_csv(dirpath, fileName):
 def save_file(df, dirpath, savefileName) :
     fileFormat = '.csv'
     df.to_csv(dirpath + savefileName + fileFormat , encoding='utf-8-sig')
+
+#csv 파일 저장
+#df: 저장할 dataframe dirpath : 저장할 위치 fileName : 저장할 파일이름
+def save_file(df, dirpath, savefileName) :
+    fileFormat = '.csv'
+    df.to_csv(dirpath + savefileName + fileFormat , encoding='utf-8-sig')
+
+#테슬라 주식 파일 읽어오기
+#dirpath : 테슬라 파일 주식 위치
+#savepath : 저장할 파일 위치
+# fileName : 저장할 파일 이름
+def daily_stock_merge_csv(dirpath,savepath,fileName):
+    
+    file_list = os.listdir(dirpath)
+    file_list_csv = [file for file in file_list if file.endswith('.csv')]
+
+    merged_df = pd.DataFrame()
+    
+    for file in file_list_csv:
+        # 파일명에서 연도 정보 추출 (예: "2022_01_news_data.csv")
+        #2020 ~ 2023년도 daily가 포함된 파일명만 merge
+        if 'daily' in file and ['2023','2022','2021','2020']:
+            print(file)
+            df = pd.read_csv(dirpath + file, dtype='object')
+            merged_df = merged_df._append(df)
+    #동일한 폴더에 병합한 csv 파일 저장
+    merged_df.to_csv(savepath + fileName + ".csv", index=False, encoding='utf-8-sig')
